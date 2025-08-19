f1f6efb1b72a8a1ee366c024eb8ca16e4c481873
jrshs79 <jrshs79@gmail.com>
2025-08-19 14:58:32 -0700

    Added the groundsys isaaclab project folder
    Added the working folder for the initial ground based system training.  Currently working through terrain generation issues by importing pre-built USD.  The folder contains a dirt_field USD to test and also references the safety_park USD which was too large to upload.  Regardless of which USD is being utilized, the file path will have to updated in the primary groundsys.py script.

---
+++ ground_sys/ground_sys/scripts/import argparse.py
@@ -0,0 +1,46 @@
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# create argparser
+parser = argparse.ArgumentParser(description="Tutorial on spawning prims into the scene.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+from isaaclab.terrains import TerrainGenereator, TerrainGeneratorCfg
+
+def design_scene():
+    cfg_terrain = TerrainGeneratorCfg(seed=42, color_scheme='random')
+    cfg_terrain.func("/home/jrshs79/IsaacLab/source/isaaclab/isaaclab/terrains", cfg_ground)
+
+def main():
+    """Main function."""
+
+    # Initialize the simulation context
+    sim_cfg = sim_utils.SimulationCfg(dt=0.01, device=args_cli.device)
+    sim = sim_utils.SimulationContext(sim_cfg)
+    # Set main camera
+    sim.set_camera_view([2.0, 0.0, 2.5], [-0.5, 0.0, 0.5])
+    # Design scene
+    design_scene()
+    # Play the simulator
+    sim.reset()
+    # Now we are ready!
+    print("[INFO]: Setup complete...")
+
+    # Simulate physics
+    while simulation_app.is_running():
+        # perform step
+        sim.step()
+
+
+if __name__ == "__main__":
+    # run the main function
+    main()
+    # close sim app
+    simulation_app.close()
